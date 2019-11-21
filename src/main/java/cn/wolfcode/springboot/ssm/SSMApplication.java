2d81e43b31610314f6f5e6ba00ee8814d98ead8f
LZC <2636050091@qq.com>
2019-11-21 13:12:27 +0800

    更新mapper接口位置

--- src/main/java/cn/wolfcode/springboot/ssm/SSMApplication.java
+++ src/main/java/cn/wolfcode/springboot/ssm/SSMApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("cn.wolfcode.springboot.ssm.mapper")
+@MapperScan("cn.wolfcode.springboot.ssm.mapper1")
 public class SSMApplication {
 
 	public static void main(String[] args) {
